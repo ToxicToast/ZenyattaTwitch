0d90e379e26ba1d737153920257efbc7a304c887
ToxicToast <info@toxictoast.de>
2018-06-14 17:16:18 +0200

    join component change

--- src/app/core/components/join/join.component.ts
+++ src/app/core/components/join/join.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'zenyatta-join',
@@ -6,28 +6,12 @@ import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
   styleUrls: ['./join.component.scss']
 })
 export class JoinComponent implements OnInit {
-  private emitPayload = {
-    channel: '',
-    username: ''
-  };
   @Input() channel: string;
   @Input() username: string;
   @Input() self: boolean;
 
-  @Output() timeoutUser = new EventEmitter<any>();
-  @Output() banUser = new EventEmitter<any>();
-  @Output() modUser = new EventEmitter<any>();
-
   constructor() { }
 
-  ngOnInit() {
-    this.emitPayload.username = this.username;
-    this.emitPayload.channel = this.channel;
-  }
-
-  onTimeoutUser() {
-    this.timeoutUser.emit(this.emitPayload);
-    console.error(this.emitPayload);
-  }
+  ngOnInit() {}
 
 }
